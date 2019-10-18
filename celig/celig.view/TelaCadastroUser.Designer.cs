5ebe340de8330da314add5694c499bde5ed58496
ivoantoniokalbuschjunior <ivoantoniokalbuschjunior@hotmail.com>
2019-10-17 21:42:50 -0300

    telas
    principal e cad usuarios

--- celig/celig.view/TelaCadastroUser.Designer.cs
+++ celig/celig.view/TelaCadastroUser.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace celig.view
 {
-    partial class FrmLogin
+    partial class TelaCadastroUser
     {
         /// <summary>
         /// Required designer variable.
@@ -28,107 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.TxtConfirmaSenha = new System.Windows.Forms.TextBox();
+            this.TxtSenha = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.TxtNome = new System.Windows.Forms.TextBox();
-            this.TxtSenha = new System.Windows.Forms.TextBox();
-            this.BtnEntrar = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.BtnCadastrar = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // TxtNome
+            // 
+            this.TxtNome.Location = new System.Drawing.Point(52, 122);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(152, 20);
+            this.TxtNome.TabIndex = 0;
+            // 
+            // TxtConfirmaSenha
+            // 
+            this.TxtConfirmaSenha.Location = new System.Drawing.Point(52, 200);
+            this.TxtConfirmaSenha.Name = "TxtConfirmaSenha";
+            this.TxtConfirmaSenha.Size = new System.Drawing.Size(152, 20);
+            this.TxtConfirmaSenha.TabIndex = 2;
+            // 
+            // TxtSenha
+            // 
+            this.TxtSenha.Location = new System.Drawing.Point(52, 161);
+            this.TxtSenha.Name = "TxtSenha";
+            this.TxtSenha.Size = new System.Drawing.Size(152, 20);
+            this.TxtSenha.TabIndex = 3;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Location = new System.Drawing.Point(49, 106);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nome de Usuário";
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome do Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 140);
+            this.label2.Location = new System.Drawing.Point(49, 145);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 5;
             this.label2.Text = "Senha";
-            // 
-            // TxtNome
-            // 
-            this.TxtNome.Location = new System.Drawing.Point(15, 117);
-            this.TxtNome.Name = "TxtNome";
-            this.TxtNome.Size = new System.Drawing.Size(267, 20);
-            this.TxtNome.TabIndex = 2;
-            // 
-            // TxtSenha
-            // 
-            this.TxtSenha.Location = new System.Drawing.Point(15, 156);
-            this.TxtSenha.Name = "TxtSenha";
-            this.TxtSenha.Size = new System.Drawing.Size(267, 20);
-            this.TxtSenha.TabIndex = 3;
-            // 
-            // BtnEntrar
-            // 
-            this.BtnEntrar.Location = new System.Drawing.Point(103, 182);
-            this.BtnEntrar.Name = "BtnEntrar";
-            this.BtnEntrar.Size = new System.Drawing.Size(90, 41);
-            this.BtnEntrar.TabIndex = 4;
-            this.BtnEntrar.Text = "Entrar";
-            this.BtnEntrar.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(38, 240);
+            this.label3.Location = new System.Drawing.Point(49, 184);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(220, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Se não possui uma conta cadastre-se";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Confirmar Senha";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // BtnCadastrar
             // 
-            this.BtnCadastrar.Location = new System.Drawing.Point(103, 270);
+            this.BtnCadastrar.Location = new System.Drawing.Point(52, 226);
             this.BtnCadastrar.Name = "BtnCadastrar";
-            this.BtnCadastrar.Size = new System.Drawing.Size(90, 41);
-            this.BtnCadastrar.TabIndex = 6;
-            this.BtnCadastrar.Text = "Cadastrar-se";
+            this.BtnCadastrar.Size = new System.Drawing.Size(92, 44);
+            this.BtnCadastrar.TabIndex = 7;
+            this.BtnCadastrar.Text = "Cadastrar";
             this.BtnCadastrar.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox1.Image = global::celig.view.Properties.Resources.usericon;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(68, 71);
+            this.pictureBox1.Size = new System.Drawing.Size(68, 81);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
-            // FrmLogin
+            // TelaCadastroUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 323);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(308, 296);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.BtnCadastrar);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.BtnEntrar);
-            this.Controls.Add(this.TxtSenha);
-            this.Controls.Add(this.TxtNome);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.MaximizeBox = false;
-            this.Name = "FrmLogin";
-            this.ShowInTaskbar = false;
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.Text = "Tela de Login";
+            this.Controls.Add(this.TxtSenha);
+            this.Controls.Add(this.TxtConfirmaSenha);
+            this.Controls.Add(this.TxtNome);
+            this.Name = "TelaCadastroUser";
+            this.Text = "TelaCadastroUser";
+            this.Load += new System.EventHandler(this.TelaCadastroUser_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -137,14 +131,13 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.TextBox TxtConfirmaSenha;
+        private System.Windows.Forms.TextBox TxtSenha;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox TxtNome;
-        private System.Windows.Forms.TextBox TxtSenha;
-        private System.Windows.Forms.Button BtnEntrar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button BtnCadastrar;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
-
---
+++ celig/celig.view/TelaPrincipal.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace celig.view
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.inserirContaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excluirContaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(4, 3);
+            this.monthCalendar1.Location = new System.Drawing.Point(128, 33);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.SelectionRange = new System.Windows.Forms.SelectionRange(new System.DateTime(2019, 12, 25, 0, 0, 0, 0), new System.DateTime(2019, 12, 31, 0, 0, 0, 0));
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 48);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(969, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(969, 24);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(969, 24);
+            this.menuStrip3.TabIndex = 3;
+            this.menuStrip3.Text = "menuStrip3";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inserirContaToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(51, 20);
+            this.toolStripMenuItem1.Text = "Inserir";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excluirContaToolStripMenuItem});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(53, 20);
+            this.toolStripMenuItem2.Text = "Excluir";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(125, 20);
+            this.toolStripMenuItem3.Text = "toolStripMenuItem3";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(125, 20);
+            this.toolStripMenuItem4.Text = "toolStripMenuItem4";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(125, 20);
+            this.toolStripMenuItem5.Text = "toolStripMenuItem5";
+            // 
+            // inserirContaToolStripMenuItem
+            // 
+            this.inserirContaToolStripMenuItem.Name = "inserirContaToolStripMenuItem";
+            this.inserirContaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.inserirContaToolStripMenuItem.Text = "inserir Conta";
+            // 
+            // excluirContaToolStripMenuItem
+            // 
+            this.excluirContaToolStripMenuItem.Name = "excluirContaToolStripMenuItem";
+            this.excluirContaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.excluirContaToolStripMenuItem.Text = "Excluir Conta";
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 525);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.menuStrip3);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TelaPrincipal";
+            this.Text = "TelaPrincipal";
+            this.Load += new System.EventHandler(this.TelaPrincipal_Load);
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem inserirContaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem excluirContaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+    }
+}
